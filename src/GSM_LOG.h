0bcee2db6b041ea9f3763781e3f95b10289c9b18
Max <sonthaya.nt@hotmail.com>
2021-08-17 03:05:52 +0700

    Add log type V

--- src/GSM_LOG.h
+++ src/GSM_LOG.h
@@ -1,3 +1,6 @@
 #define DEBUG_SERAIL Serial
 #define GSM_LOG_E(FORMAT, ...) DEBUG_SERAIL.printf("[E]: " FORMAT "\n", ## __VA_ARGS__)
 #define GSM_LOG_I(FORMAT, ...) DEBUG_SERAIL.printf("[I]: " FORMAT "\n", ## __VA_ARGS__)
+// #define GSM_LOG_I(FORMAT, ...) ;
+#define GSM_LOG_V(FORMAT, ...) DEBUG_SERAIL.printf("[V]: " FORMAT "\n", ## __VA_ARGS__)
+
