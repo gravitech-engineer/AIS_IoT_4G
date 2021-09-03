3a94aef466d44c815774396b47c5cdb60cc5e460
Max <sonthaya.nt@hotmail.com>
2021-09-03 14:13:40 +0700

    Add SHT40 (not test yet)

---
+++ src/SHT40.cpp
@@ -0,0 +1,59 @@
+#include "SHT40.h"
+
+SHT40Class::SHT40Class(TwoWire *wire) {
+    this->wire = wire;
+}
+
+bool SHT40Class::begin(uint8_t address) {
+    this->dev_addr = address;
+}
+
+bool SHT40Class::read() {
+    this->wire->beginTransmission(this->dev_addr);
+    this->wire->write(0xFD);
+    this->wire->endTransmission();
+
+    delay(10);
+
+    int n = this->wire->requestFrom(this->dev_addr, 6);
+    if (n != 6) {
+        return false;
+    }
+
+    uint8_t rx_bytes[6];
+    this->wire->readBytes(rx_bytes, 6);
+
+    uint16_t t_ticks = rx_bytes[0] * 256 + rx_bytes[1];
+    uint8_t checksum_t = rx_bytes[2];
+    uint16_t rh_ticks = rx_bytes[3] * 256 + rx_bytes[4];
+    uint8_t checksum_rh = rx_bytes[5];
+    this->t_degC = -45 + 175 * t_ticks/65535;
+    this->rh_pRH = -6 + 125 * rh_ticks/65535;
+
+    this->rh_pRH = max(min(rh_pRH, 100.0f), 0.0f);
+
+    return true;
+}
+
+float SHT40Class::readTemperature(int units) {
+    if (!this->read()) {
+        return -99;
+    }
+
+    return units == CELSIUS ? this->t_degC : (this->t_degC * 9.0 / 5.0 + 32.0);
+}
+
+float SHT40Class::readHumidity() {
+    if (!this->read()) {
+        return -99;
+    }
+
+    return this->rh_pRH;
+}
+
+bool SHT40Class::end() {
+    this->wire = NULL;
+    this->dev_addr = 0;
+}
+
+SHT40Class SHT40(&Wire);
---
+++ src/SHT40.h
@@ -0,0 +1,34 @@
+#ifndef __SHT40_H__
+#define __SHT40_H__
+
+#include "Arduino.h"
+#include "Wire.h"
+
+enum {
+    CELSIUS,
+    FAHRENHEIT
+};
+
+class SHT40Class {
+    private:
+        TwoWire *wire = NULL;
+        uint8_t dev_addr = 0x00;
+
+        float t_degC;
+        float rh_pRH;
+
+        bool read() ;
+
+    public:
+        SHT40Class(TwoWire *wire = &Wire) ;
+
+        bool begin(uint8_t address = 0x44) ;
+        float readTemperature(int units = CELSIUS) ;
+        float readHumidity() ;
+        bool end() ;
+
+};
+
+extern SHT40Class SHT40;
+
+#endif
