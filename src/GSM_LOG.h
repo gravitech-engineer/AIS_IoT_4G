7db9302957f251152c902a8952734fc514635e9c
Max <sonthaya.nt@hotmail.com>
2021-09-08 15:54:18 +0700

    Add GSM_LOG_W()

--- src/GSM_LOG.h
+++ src/GSM_LOG.h
@@ -4,9 +4,11 @@
 #define GSM_LOG_E(FORMAT, ...) DEBUG_SERAIL.printf("[E]: " FORMAT "\n", ## __VA_ARGS__)
 
 #ifdef DEBUG_GSM
+#define GSM_LOG_W(FORMAT, ...) DEBUG_SERAIL.printf("[W]: " FORMAT "\n", ## __VA_ARGS__)
 #define GSM_LOG_I(FORMAT, ...) DEBUG_SERAIL.printf("[I]: " FORMAT "\n", ## __VA_ARGS__)
 #define GSM_LOG_V(FORMAT, ...) DEBUG_SERAIL.printf("[V]: " FORMAT "\n", ## __VA_ARGS__)
 #else
+#define GSM_LOG_W(FORMAT, ...) ;
 #define GSM_LOG_I(FORMAT, ...) ;
 #define GSM_LOG_V(FORMAT, ...) ;
 #endif
