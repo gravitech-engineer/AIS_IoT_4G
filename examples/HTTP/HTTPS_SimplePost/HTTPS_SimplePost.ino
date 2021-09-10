58754f307a2c917231b803c11309493fbfd84df5
Max <sonthaya.nt@hotmail.com>
2021-09-10 13:45:19 +0700

    Add HTTPS_SimplePost

---
+++ examples/HTTP/HTTPS_SimplePost/HTTPS_SimplePost.ino
@@ -0,0 +1,78 @@
+/*
+  Simple POST client for ArduinoHttpClient library
+  Connects to server once every five seconds, sends a POST request
+  and a request body
+
+  created 14 Feb 2016
+  modified 22 Jan 2019
+  by Tom Igoe
+  
+  this example is in the public domain
+ */
+#include <SIM76xx.h>
+#include <GSMClientSecure.h>
+#include <ArduinoHttpClient.h> // Required ArduinoHttpClient, Install via Library manager
+
+const char *serverAddress = "reqres.in";  // server address
+int port = 443;
+
+const char *root_digi_cert = \
+"-----BEGIN CERTIFICATE-----\n"
+"MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\n"
+"RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\n"
+"VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX\n"
+"DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y\n"
+"ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy\n"
+"VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr\n"
+"mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr\n"
+"IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK\n"
+"mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu\n"
+"XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy\n"
+"dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye\n"
+"jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1\n"
+"BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3\n"
+"DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92\n"
+"9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx\n"
+"jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0\n"
+"Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\n"
+"ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\n"
+"R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\n"
+"-----END CERTIFICATE-----\n";
+
+GSMClientSecure gsmClient;
+HttpClient client = HttpClient(gsmClient, serverAddress, port);
+
+void setup() {
+  Serial.begin(115200);
+
+  while (!GSM.begin()) { // Setup GSM (Power on and wait GSM ready)
+      Serial.println("GSM setup fail");
+      delay(2000);
+  }
+
+  gsmClient.setCACert(root_digi_cert); // Set CA Certificate
+}
+
+void loop() {
+  Serial.println("making POST request");
+  String contentType = "application/json";
+  String postData = \
+  "{"
+  "  \"name\": \"morpheus\","
+  "  \"job\": \"leader\""
+  "}";
+
+  client.post("/api/users", contentType, postData);
+
+  // read the status code and body of the response
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+
+  Serial.println("Wait five seconds");
+  delay(5000);
+}
