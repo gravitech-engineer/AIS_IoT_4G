680a346f347da969f453eb5e04e51062efe043be
Max <sonthaya.nt@hotmail.com>
2021-09-14 01:51:47 +0700

    Add try to fix  error by self

--- src/GSMSocket.cpp
+++ src/GSMSocket.cpp
@@ -16,11 +16,23 @@ void setup_Socket() {
         int socket_id = -1;
         if (sscanf(urcText.c_str(), "+CIPRXGET: 1,%d", &socket_id) != 1) {
             GSM_LOG_E("+CIPRXGET: 1 format fail");
+            GSM_LOG_I("Triger read_request_flag all socket for fixed");
+            for (uint8_t i=0;i<10;i++) {
+                if (ClientSocketInfo[i].itUsing) {
+                    ClientSocketInfo[i].read_request_flag = true;
+                }
+            }
             return;
         }
 
         if ((socket_id < 0) || (socket_id > 9)) {
             GSM_LOG_E("+CIPRXGET: 1 | Socket %d is out of range", socket_id);
+            GSM_LOG_I("Triger read_request_flag all socket for fixed");
+            for (uint8_t i=0;i<10;i++) {
+                if (ClientSocketInfo[i].itUsing) {
+                    ClientSocketInfo[i].read_request_flag = true;
+                }
+            }
             return;
         }
 
