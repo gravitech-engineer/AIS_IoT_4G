6440d58df03299e2c0a804278bcf8add2fd078b3
AIS IoT Platform Integration <113405874+AIS-DeviceIntegration@users.noreply.github.com>
2022-09-20 14:50:01 +0700

    Update reportSensorJSON.ino

--- examples/Magellan/MQTT/reportSensor/reportSensorJSON/reportSensorJSON.ino
+++ examples/Magellan/MQTT/reportSensor/reportSensorJSON/reportSensorJSON.ino
@@ -20,7 +20,7 @@ void loop()
   {
     magel.subscribe.report.response(); // optional register for get Resp report
   });
-  magel.interval(10, [](){   
+  magel.interval(10, [](){   //time interval function inside every 10 second
     //{1} 
     delay(50);
     String Temperature = String(magel.builtInSensor.readTemperature());
